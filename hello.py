050dbba1dd078c3f725d1f0d18147699e339f884
codecreator <alexmarseen@yandex.ru>
2017-08-01 17:42:36 +0300

    hll

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
 def app(environ, start_response):
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+    return [data]
