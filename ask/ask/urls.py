98f154fdd80309b539f840ec71746b0495072f02
codecreator <alexmarseen@yandex.ru>
2017-08-03 19:29:03 +0300

    d

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/$', 'qa.views.test'),
-    url(r'^signup/$', 'qa.views.test'),
-    url(r'^question/(\d+)$', 'qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/$', 'qa.views.test'),
-    url(r'^new/$', 'qa.views.test'),
+urlpatterns = patterns('qa.views',
+    url(r'^$', 'test'),
+    url(r'^login/*$', 'test'),
+    url(r'^signup/*$', 'test'),
+    url(r'^question/(\d+)$', 'test'),
+    url(r'^ask/.*$', 'test'),
+    url(r'^popular/*$', 'test'),
+    url(r'^new/*$', 'test'),
 )
--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo gunicorn -c /etc/gunicorn.d/test hello:application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application﻿
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi
 sudo /etc/init.d/gunicorn restart
