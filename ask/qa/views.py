d3d49c949e04786eb9c504e627a9a968f149e73c
codecreator <alexmarseen@yandex.ru>
2017-08-03 16:46:06 +0300

    2

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-from django.http import HttpResponse 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+gunicorn -c /home/box/web/etc/gunicorn-django.conf wsgi
 sudo /etc/init.d/gunicorn restart
