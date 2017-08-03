1a5fccdfc7ae9c679bc271232917492c308294e6
codecreator <alexmarseen@yandex.ru>
2017-08-03 14:04:46 +0300

    urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,9 +1,9 @@
 urlpatterns = [
-    url(r'^$', question_list, name='question_list'),
-    url(r'^login/', user_login, name='login'),
-    url(r'^signup/', user_signup, name='signup'),
-    url(r'^ask/', question_ask, name='question_ask'),
-    url(r'^popular/', popular, name='popular'),
-    url(r'^new/', test, name='new'),
-    url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
+    url(r'^$', include('qa.urls')),
+    url(r'^login/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls')),
+    url(r'^question/(?P<pk>\d+)/', include('qa.urls')),
 ]
