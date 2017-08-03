842a4a0333fc8adafc9dbba57618d21aacc3fdca
codecreator <alexmarseen@yandex.ru>
2017-08-03 10:49:02 +0300

    init

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn -c /etc/gunicorn.d/test hello:application
+sudo gunicorn -c /etc/gunicorn.d/test hello:app
 sudo /etc/init.d/gunicorn restart
