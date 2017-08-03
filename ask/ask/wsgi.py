bbe5fc5f6e507532aacefdefa3eae320cfb06d23
codecreator <alexmarseen@yandex.ru>
2017-08-03 15:30:02 +0300

    upd

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -1,18 +1,14 @@
 """
 WSGI config for ask project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/home/box/web/ask')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
 application = get_wsgi_application()
