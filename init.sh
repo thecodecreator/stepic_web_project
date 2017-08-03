a2a4e7fe724a8c2e7d0508cd01cbf5d42cab611f
codecreator <alexmarseen@yandex.ru>
2017-08-03 20:09:22 +0300

    i

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn -c /etc/gunicorn.d/test hello:application
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/ask
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
